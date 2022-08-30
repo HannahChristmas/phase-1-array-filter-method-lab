e847a6a63e0986465ef29491f945724d667e1d63
Hannah Christmann <hc109909@gmail.com>
2022-08-30 14:27:32 -0400

    commit tree 5c0b7d67cfd18642650b231cb10f9797f6569fa2

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, name){
+    let result = drivers.filter(item => item.toLowerCase() === name.toLowerCase())
+    if(result){
+        return result
+    }
+}
+
+function fuzzyMatch(drivers, name){
+    let result = drivers.filter(item => item[0] === name[0])
+    if(result){
+        return result
+    }
+}
+
+function matchName(drivers, name){
+    let result = drivers.filter(item => item.name === name)
+    if(result){
+        return result
+    }
+}
